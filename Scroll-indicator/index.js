6e9595b33b21fda142384821899f3ce4d5c82925
SahilButala <sahil06butala@gmail.com>
2024-08-19 22:13:16 +0530

    fifth project

---
+++ Scroll-indicator/index.js
@@ -0,0 +1,39 @@
+const scrollbar = document.querySelector(".progress-bar");
+console.log(scrollbar)
+const scrollcontent = document.querySelector(".scroll-content");
+
+function fetchData() {
+  try {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((res) => res.json())
+      .then((data) => {
+        displayData(data);
+      });
+  } catch (e) {
+    console.log("error", e.message);
+  }
+}
+function displayData(getData){
+    console.log(getData)
+    scrollcontent.innerHTML  = getData.map((item)=>{
+        console.log(item)
+        return `<div class="box">
+          <p>${item.description.slice(0,200)+ "...."}</p>
+          <h5 class="title">${item.title}</h5>
+          <div class="price">${item.price}$</div>
+        </div>`
+    }).join("")
+}
+fetchData();
+
+
+window.onscroll = function(){
+      handleScroll()
+}
+function handleScroll(){
+    let topheight = document.body.scrollTop || document.documentElement.scrollTop
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+
+    let alreadyScrolled = (topheight/height)*100
+    scrollbar.style.width = `${alreadyScrolled}%`
+}
